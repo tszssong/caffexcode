e0c9fafa7e2e566a8ddca0c52e7e7fb7ebc783e3
zheng.meisong <zheng.meisong@immomo.com>
2018-10-29 12:55:58 +0800

    init

---
+++ caffeTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  caffeTest
+//
+//  Created by MOMO on 2018/5/11.
+//  Copyright © 2018年 MOMO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
